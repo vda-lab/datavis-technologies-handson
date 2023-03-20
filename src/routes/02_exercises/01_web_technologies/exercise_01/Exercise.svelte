b82dc1e62eef7f68d8c61010b2a9314030c220be
makdong <dhkim0317@postech.ac.kr>
2023-03-20 14:13:59 +0100

    Exercise 1, 2

--- src/routes/02_exercises/01_web_technologies/exercise_01/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_01/Exercise.svelte
@@ -1,7 +1,1 @@
-<div class="answer">My name is ...</div>
-
-<style>
-  div.answer {
-    color: rgb(2, 96, 45);
-  }
-</style>
+<div class="answer" style="color: Red;">My name is <b>Harbour Kim</b></div>
---
+++ src/routes/02_exercises/01_web_technologies/exercise_03/Exercise.svelte
@@ -0,0 +1,4 @@
+<div>The best visual channels ordered by Mackinlay's ranking:</div>>
+<li>position</li>
+<li>length</li>
+<li>angle</li>
