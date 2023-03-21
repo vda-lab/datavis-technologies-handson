b37a2205a6633123375a0ff48cac864bca752c1e
Langsi97 <langsiambe@gmail.com>
2023-03-21 01:42:55 +0100

    9 and 10

---
+++ src/routes/02_exercises/01_web_technologies/exercise_09/Exercise.svelte
@@ -0,0 +1,10 @@
+<script>
+function  circum_sphere(radius){  
+  const circumference=2*Math.PI*radius;
+   return circumference
+}
+
+const radius = 5;
+const circumference =circum_sphere(radius);
+</script>
+<p>'The circumference of a circle with radius {radius}m is {circumference.toFixed(2)}m.'</p>
---
+++ src/routes/02_exercises/01_web_technologies/exercise_10/Exercise.svelte
@@ -0,0 +1,9 @@
+<div>
+  <Exercise_03/>
+  <Exercise_03/>
+</div>
+
+<script>
+  import Exercise_03 from './Exercise_03.svelte';
+  import Exercise_02from './Exercise_02.svelte';
+</script>
