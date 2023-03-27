dd6547bea957ec5301d3b9b22122648bedb13319
ChrissiKalk <christina.kalk@web.de>
2023-03-27 16:48:19 +0200

    ex05 and 06

--- src/routes/02_exercises/01_web_technologies/exercise_06/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_06/Exercise.svelte
@@ -1,1 +1,10 @@
-I really like data visualisation!
+<p>I <span style='color: pink'>really</span> like data visualisation!</p>
+
+<style>
+  p {
+    color: steelblue;
+    font-family: verdana;
+    font-size: 300%;
+    
+  }
+</style>
