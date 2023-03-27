f0d7da9a3c39cde51b74dfdb66bd6682858c48cc
ChrissiKalk <christina.kalk@web.de>
2023-03-27 17:33:18 +0200

    ex 7 and 10

--- src/routes/02_exercises/01_web_technologies/exercise_07/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_07/Exercise.svelte
@@ -1,1 +1,4 @@
-My lucky number is
+<script>
+const lucky_nr = 23
+</script>
+My lucky number is {lucky_nr}
---
+++ src/routes/02_exercises/01_web_technologies/exercise_10/Exercise.svelte
@@ -0,0 +1,4 @@
+<script>import E2 from '/home/ChrissiKalk/datavis-technologies-handson/src/routes/02_exercises/01_web_technologies/exercise_02/Exercise.svelte'
+import E3 from '/home/ChrissiKalk/datavis-technologies-handson/src/routes/02_exercises/01_web_technologies/exercise_03/Exercise.svelte' </script>
+<div><E3 />
+<E2 /></div>
