4a6ef09a129d3421fa46fc6cb1384ad546144c0a
ChrissiKalk <christina.kalk@web.de>
2023-04-11 21:46:13 +0200

    exercise 9 finally working

--- src/routes/02_exercises/01_web_technologies/exercise_09/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_09/Exercise.svelte
@@ -1,9 +1,9 @@
 <script>
 	function circ(radius) {
-  return Math.PI * radius *  2;
-  let rad = 2;
-  let circum = circ(rad);
+  return Number((Math.PI * radius *  2).toFixed(2));
 }
+const rad = 2;
+const circum = circ(rad);
 </script>
 
 <p>The circumference of a circle with radius {rad}m is {circum}m .</p>
