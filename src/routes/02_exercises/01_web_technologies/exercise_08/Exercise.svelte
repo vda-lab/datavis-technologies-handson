f75fb6e7b27048394b992a0f685ecdaeac1e66c9
ChrissiKalk <christina.kalk@web.de>
2023-04-11 20:46:12 +0200

    Ex 12 now working

--- src/routes/02_exercises/01_web_technologies/exercise_08/Exercise.svelte
+++
@@ -1,3 +0,0 @@
-<script>my_fav_things =['Cappuccino', 'dancing', 'sports', 'my laptop']</script>
-<p>"My name is Christina and these are some of my favourite things:
-console.log(my_fav_things)."</p>
--- src/routes/02_exercises/01_web_technologies/exercise_12/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_12/Exercise.svelte
@@ -6,8 +6,8 @@
 	<button class="btn btn-primary" on:click={() => (clicked = true)}>
 		click here
 	</button>
-	<!-- This is not working but ÄI dont know why... -->
-	{#if clicked == "F"}
+	<!-- This is not working but I dont know why... -->
+	{#if clicked == false}
 		<p>'You have to click the button!'</p>
 	{:else}
 		<p>'You have clicked the button!'</p>
