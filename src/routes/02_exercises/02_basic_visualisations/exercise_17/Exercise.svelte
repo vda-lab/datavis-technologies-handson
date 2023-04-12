891ef57cc91a5bf2fd9bb4fd38eafec7f69921d7
ChrissiKalk <christina.kalk@web.de>
2023-04-12 19:44:21 +0200

    Ex 17: does not look as expected...

--- src/routes/02_exercises/02_basic_visualisations/exercise_17/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_17/Exercise.svelte
@@ -1,7 +1,6 @@
 <script>
-import { scaleLinear } from 'd3-scale';
-const scale = scaleLog().domain([1, 10])
-            .range([10, 20, 30, 40, 50, 60]);
+import { scaleLog } from 'd3-scale';
+const scale = scaleLog().domain([2, 9]).range([0,1]);
 
     // Dimensions
     const width = 800;
@@ -13,15 +12,15 @@ const scale = scaleLog().domain([1, 10])
     // Array
     const values = [2, 4, 6, 7, 9];
     let log_vals = [0, 0, 0, 0, 0];
-    for ( var i = 0; i < 5; i++ ) {
-      log_vals[i] = values[i];
+    for ( let i = 0; i < 5; i++ ) {
+      log_vals[i] = scale(values[i]);
 };
     
   </script>
   
   <svg viewBox="0 0 {width} {height}">
     <g transform="translate({margin.left},{margin.top})">
-        {#each log_values as dp}
+        {#each log_vals as dp}
     <circle cx={dp} cy="0" r=10 />
   {/each}
     </g>
