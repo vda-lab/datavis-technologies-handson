738eaebd2dce26db3787338128522fa916da1b45
ChrissiKalk <christina.kalk@web.de>
2023-04-15 12:35:57 +0200

    ex_18

--- src/routes/02_exercises/02_basic_visualisations/exercise_18/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_18/Exercise.svelte
@@ -1,4 +1,17 @@
 <script>
+import { scaleLog } from 'd3-scale';
+import { axisBottom } from 'd3-axis';
+import { select } from 'd3-selection';
+
+const scale = scaleLog().domain([2, 9]).range([10,780]);
+const xAxis = axisBottom(scale);
+
+
+
+function executeXAxis(handle){
+    xAxis(select(handle));
+  }
+
     // Dimensions
     const width = 800;
     const height = 100;
@@ -8,11 +21,32 @@
   
     // Array
     const values = [2, 4, 6, 7, 9];
+    const mappedValues = values.map(v => scale(v));
+
+    
   </script>
   
   <svg viewBox="0 0 {width} {height}">
     <g transform="translate({margin.left},{margin.top})">
-      <!--  -->
+      {#each mappedValues as dp}
+        <circle cx={dp} cy="50" r=10 />
+      {/each}
     </g>
+    <g use:executeXAxis  transform="translate({margin.left},{height-margin.bottom})">
+    
+    </g>
+    <text x="395" y="100" class="x-label">x-axis</text>
   </svg>
+
+  <style>
+    .x-label{
+      fill: blue;
+      font-size: 10px;
+    }
+  </style>
+
+
+
+
+
   
